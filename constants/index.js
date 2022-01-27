7898d8ce4bbab2ce822c591e9e9f33be198d1230
nazifsohtaoglu <sohtanazif@gmail.com>
2022-01-27 18:41:56 +0300

    initial

---
+++ constants/index.js
@@ -0,0 +1,66 @@
+export const abi = [
+    {
+        "inputs": [
+            {
+                "internalType": "uint16",
+                "name": "_maxWhitelistedAddresses",
+                "type": "uint16"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "addAddressToWhitelist",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "maxWhitelistedAddresses",
+        "outputs": [
+            {
+                "internalType": "uint16",
+                "name": "",
+                "type": "uint16"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "numAddressesWhitelisted",
+        "outputs": [
+            {
+                "internalType": "uint16",
+                "name": "",
+                "type": "uint16"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "whitelistedAddresses",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
+export const WHITELIST_CONTRACT_ADDRESS = "0xa4b8ABa535029FA6BF950A0137BB612BFfD380E4";
